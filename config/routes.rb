215d4127cade520b26cd81bfb163f2d1be8f3cc2
evelyn <astudillobarra.evelyn@gmail.com>
2018-07-19 21:29:12 -0400

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :productos
+  resources :products
   get 'pages/home'
   get 'pages/about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
